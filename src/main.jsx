4d53eecbc42a371ffed04dd519557fcec041c41c
jost <johan.stettler@gmail.com>
2025-03-28 14:05:57 +0100

    debut timline package 2

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,12 @@ import App from './App'
 
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { enUS } from 'date-fns/locale'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={enUS}>
         <App />
       </LocalizationProvider>
     </BrowserRouter>
