44f1322ecbe1e3e46d5637b5c3235280eb5883a8
jost <johan.stettler@gmail.com>
2025-05-02 16:20:41 +0200

    debug negative view and edit

--- src/pages/NegativeViewEdit.jsx
+++ src/pages/NegativeViewEdit.jsx
@@ -159,7 +159,7 @@ export default function NegativeViewEdit() {
                   <TableCell>{incident.visibility}</TableCell>
                   <TableCell>
                     <Stack spacing={1} direction="row">
-                      {(story.owner === role) && !isViewer && (
+                      {(incident.owner === role) && !isViewer && (
                         <>
                           <Button variant="outlined" size="small" onClick={() => handleEdit(index)}>
                             Edit
@@ -169,12 +169,12 @@ export default function NegativeViewEdit() {
                           </Button>
                         </>
                       )}
-                      {(story.owner !== role || isViewer) && (
+                      {(incident.owner !== role || isViewer) && (
                         <Button variant="outlined" size="small" onClick={() => handleView(index)}>
                           View
                         </Button>
                       )}
-                      <Button variant="outlined" size="small" onClick={() => handleDownloadOne(story, index)}>
+                      <Button variant="outlined" size="small" onClick={() => handleDownloadOne(incident, index)}>
                         ⬇️ JSON
                       </Button>
                     </Stack>
